19f4a7cb301033dde5e8a7ffbaf63633ec4c949b
priyanshuu01 <122719179+priyanshuu01@users.noreply.github.com>
2025-07-09 17:45:19 +0530

    Update assets.js

--- admin/vite-project/src/assets/assets.js
+++ admin/vite-project/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-website-7zb6.onrender.com'
+export const url = 'https://food-delivery-backend-5rn1.onrender.com'
